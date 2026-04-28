709595003a8abc594d6c17b921d725ae8ee4d7bc
koleti_sankeerth <koleti_sankeerth@epam.com>
2026-04-28 11:24:36 +0530

    done

---
+++ src/main/java/com/example/jenikinsPractise/JenikinsPractiseApplication.java
@@ -0,0 +1,13 @@
+package com.example.jenikinsPractise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenikinsPractiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenikinsPractiseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jenikinsPractise/JenikinsPractiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenikinsPractise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenikinsPractiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
